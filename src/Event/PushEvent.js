3478b9b7f96a575afc0bfcd3b3715d7654da6269
Aaron Scherer <aequasi@gmail.com>
2016-02-16 15:05:51 -0800

    Fixing

--- src/Event/PushEvent.js
+++ src/Event/PushEvent.js
@@ -9,7 +9,7 @@ class PushEvent extends AbstractEvent {
         let message = `**${this.request.repository.name}** - *New Commit(s)*\n`;
 
         message += `${this.request.distinct_size} commit(s) - Head: ${this.request.head_commit.message}\n`;
-        message += `<${issue.url}>`;
+        message += `<${this.request.repository.http_url}>`;
 
         this.client.sendMessage(this.channel, message);
     }
